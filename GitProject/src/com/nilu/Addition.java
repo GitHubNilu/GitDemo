98123e2c81e85f91251645affccfb4a35ea14b39
ck <ck@192.168.0.7>
2018-06-10 16:11:27 +0530

    dev----changes value

--- GitProject/src/com/nilu/Addition.java
+++ GitProject/src/com/nilu/Addition.java
@@ -10,7 +10,9 @@ public class Addition {
 	public static void main(String[] args) {
 		System.out.println("In main");
 		System.out.println("Addition is: "+sum(100,200));
-
+		System.out.println("Addition is: "+sum(100,500));
+	//add new value
 	}
+	
 
 }
