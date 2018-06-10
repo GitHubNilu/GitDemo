5f3dcf16a0364c327a7e9badecc2ba0357cdd1c4
ck <ck@192.168.0.7>
2018-06-10 18:05:31 +0530

    trashing

--- GitProject/src/com/nilu/Substraction.java
+++ GitProject/src/com/nilu/Substraction.java
@@ -4,15 +4,16 @@ public class Substraction {
 
 	public static void main(String[] args) {
 		Substraction sub =new Substraction();
-		int result=sub.subtract(60,32);
-		System.out.println("Substraction is :"+result);
+	//	int result=sub.subtract(60,32);
+	//	System.out.println("Substraction is :"+result);
+		System.out.println("Substraction is :");
 	}
 
-	private int subtract(int a, int b ) {
+	/*private int subtract(int a, int b ) {
 		
 		return a-b;// change variable name
 		
 		
 	}
-
+*/
 }
