f453f11e426ef5bbe20f7323ef4579c3c96ab78e
ck <ck@192.168.0.7>
2018-06-10 17:19:19 +0530

    dev1---Multiplication class

---
+++ GitProject/src/com/nilu/Multiplication.java
@@ -0,0 +1,26 @@
+package com.nilu;
+
+public class Multiplication {
+	
+	static int multiplication(int x,int y)
+	{
+		return x*y;
+	}
+	public static void main(String[] args) {
+	
+		//Multiplication m1=new Multiplication();
+		
+		
+		
+		System.out.println("Multiplication :"+multiplication(10,20));
+		 
+		
+		
+		
+		
+	
+	
+
+	}
+
+}
